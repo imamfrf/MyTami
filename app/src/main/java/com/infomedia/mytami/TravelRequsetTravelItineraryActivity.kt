749161024b0b8ae4611e15f53648e8f858544585
Martin Anugrah Siahaan <38404217+martinanugrah24@users.noreply.github.com>
2019-06-25 12:29:47 +0700

    travel itinerary layout.

--- app/src/main/java/com/infomedia/mytami/TravelRequsetTravelItineraryActivity.kt
+++ app/src/main/java/com/infomedia/mytami/TravelRequsetTravelItineraryActivity.kt
@@ -3,11 +3,11 @@ package com.infomedia.mytami
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class TravelRequestTravelTypeActivity : AppCompatActivity() {
+class TravelRequsetTravelItineraryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_travel_request_travel_type)
+        setContentView(R.layout.activity_travel_requset_travel_itinerary)
         setSupportActionBar(findViewById(R.id.toolbar))
     }
 }
