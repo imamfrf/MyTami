5774510bb6ba870f36641e57a4ebba7c014a4d83
Martin Anugrah Siahaan <38404217+martinanugrah24@users.noreply.github.com>
2019-06-25 22:59:50 +0700

    travel description layout fix.

--- app/src/main/java/com/infomedia/mytami/TravelRequestTravelDescriptionActivity.kt
+++ app/src/main/java/com/infomedia/mytami/TravelRequestTravelDescriptionActivity.kt
@@ -3,8 +3,12 @@ package com.infomedia.mytami
 import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_travel_request_travel_description.*
+import net.cachapa.expandablelayout.ExpandableLayout
+import net.cachapa.expandablelayout.ExpandableLayout.OnExpansionUpdateListener
+
 
 class TravelRequestTravelDescriptionActivity : AppCompatActivity() {
 
@@ -16,6 +20,20 @@ class TravelRequestTravelDescriptionActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         toolbar.navigationIcon?.setColorFilter(resources.getColor(R.color.md_white_1000), PorterDuff.Mode.SRC_ATOP)
+
+        expandable_layout_0.setOnExpansionUpdateListener(object : OnExpansionUpdateListener {
+            override fun onExpansionUpdate(expansionFraction: Float, state: Int) {
+                Log.d("ExpandableLayout0", "State: $state")
+            }
+        })
+
+        expand_button.setOnClickListener {
+            if (expandable_layout_0.isExpanded) {
+                expandable_layout_0.collapse()
+            } else {
+                expandable_layout_0.expand()
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
