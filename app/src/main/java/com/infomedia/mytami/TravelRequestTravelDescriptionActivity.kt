217c04b664241b1a28b61cd92a24a1afaf47f423
Martin Anugrah Siahaan <38404217+martinanugrah24@users.noreply.github.com>
2019-06-26 14:15:42 +0700

    fix expandable layout.

--- app/src/main/java/com/infomedia/mytami/TravelRequestTravelDescriptionActivity.kt
+++ app/src/main/java/com/infomedia/mytami/TravelRequestTravelDescriptionActivity.kt
@@ -8,9 +8,14 @@ import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_travel_request_travel_description.*
 import net.cachapa.expandablelayout.ExpandableLayout
 import net.cachapa.expandablelayout.ExpandableLayout.OnExpansionUpdateListener
+import androidx.core.view.ViewCompat.animate
+import android.R.attr.rotation
+import android.R.attr.start
 
 
 class TravelRequestTravelDescriptionActivity : AppCompatActivity() {
+
+    private var mRotationAngle: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +38,8 @@ class TravelRequestTravelDescriptionActivity : AppCompatActivity() {
             } else {
                 expandable_layout_0.expand()
             }
+            mRotationAngle = if (mRotationAngle == 0) 180 else 0
+            imageView_expand_arrow.animate().rotation(mRotationAngle.toFloat()).setDuration(500).start()
         }
     }
 
