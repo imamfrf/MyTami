54c076b15f2fa9c91b47ec840eff42bae24802d3
panjikrisnayasa <panjikrisnayasa@gmail.com>
2019-07-04 10:33:48 +0700

    choose hotel and room

---
+++ app/src/main/java/com/infomedia/mytami/travel_request/HotelDetailActivity.java
@@ -0,0 +1,18 @@
+package com.infomedia.mytami.travel_request;
+
+import android.app.ActionBar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.infomedia.mytami.R;
+
+public class HotelDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotel_detail);
+    }
+}
---
+++ app/src/main/java/com/infomedia/mytami/travel_request/RoomDetailActivity.java
@@ -0,0 +1,14 @@
+package com.infomedia.mytami.travel_request;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.infomedia.mytami.R;
+
+public class RoomDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_detail);
+    }
+}
